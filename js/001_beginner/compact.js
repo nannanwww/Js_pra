1f7104134256943bf3fc76b0e03732278cc7e09b
nannanwww <engineerookie@gmail.com>
2025-08-28 22:22:34 +0900

    fix:index.html&js

--- js/001_beginner/compact.js
+++ js/001_beginner/compact.js
@@ -1,1 +1,17 @@
-console.log('compact関数の実装');
+function compact(array){
+
+    const compacteadArray = [];
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i]){
+            compacteadArray.push(array[i]);
+        }
+    }
+
+    return compacteadArray;
+}
+
+const compactedValues = compact([0,1,false,2,'',3]);
+console.log(compactedValues);
+
+document.getElementById("result").textContent = compactedValues;
